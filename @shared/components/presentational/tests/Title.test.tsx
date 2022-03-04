84fc0767452e64271e0d6c07b68eaae5bc5a8bda
Aken Mehta <mehta.aken@gmail.com>
2022-03-04 16:21:51 -0500

    🔧 Config jest

---
+++ @shared/components/presentational/tests/Title.test.tsx
@@ -0,0 +1,7 @@
+import { Title } from "../Title";
+import { render } from "@testing-library/react";
+
+test('renders a message', () => {
+  const { getByText } = render(<Title type="h1">Hello World</Title>);
+  expect(getByText('Hello World')).toBeInTheDocument();
+});
