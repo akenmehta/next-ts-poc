0dcdaa4494659a26081f84571cd3c83046032a34
Aken Mehta <mehta.aken@gmail.com>
2022-03-08 09:38:46 -0500

    failing test

--- @shared/components/presentational/tests/Title.test.tsx
+++ @shared/components/presentational/tests/Title.test.tsx
@@ -2,6 +2,6 @@ import { Title } from '../Title';
 import { render } from '@testing-library/react';
 
 test('renders a message', () => {
-  const { getByText } = render(<Title type="h1">Hello World</Title>);
+  const { getByText } = render(<Title type="h1">Hello Worldd</Title>);
   expect(getByText('Hello World')).toBeInTheDocument();
 });
